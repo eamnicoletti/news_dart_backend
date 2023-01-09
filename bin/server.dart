bb93759cb630bc5e7bc1ec5a57c4c8d7ef1be506
Emilio Nicoletti <nico@pop-os.localdomain>
2023-01-09 19:47:34 -0300

    create the custom server for inicialization

---
+++ bin/server.dart
@@ -0,0 +1,22 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+import 'infra/custom_server.dart';
+
+// Configure routes.
+final _router = Router()
+  ..get('/', _rootHandler)
+  ..get('/echo/<message>', _echoHandler);
+
+Response _rootHandler(Request req) {
+  return Response.ok('Hello, World!\n');
+}
+
+Response _echoHandler(Request request) {
+  final message = request.params['message'];
+  return Response.ok('$message\n');
+}
+
+void main(List<String> args) async {
+  CustomServer().initialize(_router);
+}
