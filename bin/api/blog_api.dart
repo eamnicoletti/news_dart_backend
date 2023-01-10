08ddf76306e66628b53755f04396ef1ee3cf9224
Emílio Nicoletti <eamnicoletti@gmail.com>
2023-01-10 18:49:38 -0300

    create CRUD routers to the blog_api

--- bin/api/blog_api.dart
+++ bin/api/blog_api.dart
@@ -5,7 +5,23 @@ class BlogApi {
   Handler get handler {
     Router router = Router();
 
+    // listagem
     router.get('/blog/noticias', (Request req) {
+      return Response.ok('Choveu hoje');
+    });
+
+    // nova noticia
+    router.post('/blog/noticias', (Request req) {});
+
+    // /blog/noticias?id=1 // update
+    router.put('/blog/noticias', (Request req) {
+      String? id = req.url.queryParameters['id'];
+      return Response.ok('Choveu hoje');
+    });
+
+    // /blog/noticias?id=1 // delete
+    router.delete('/blog/noticias', (Request req) {
+      String? id = req.url.queryParameters['id'];
       return Response.ok('Choveu hoje');
     });
 
