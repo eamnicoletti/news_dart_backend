e135d7ce8341cd289abf5dfbce7b21e6b1ad1020
Emílio Nicoletti <eamnicoletti@gmail.com>
2023-01-20 22:57:36 -0300

    create db connection abstraction

--- bin/server.dart
+++ bin/server.dart
@@ -1,10 +1,9 @@
-import 'package:mysql1/mysql1.dart';
 import 'package:shelf/shelf.dart';
-import 'package:shelf_router/shelf_router.dart';
 
 import 'apis/blog_api.dart';
 import 'apis/login_api.dart';
 import 'infra/custom_server.dart';
+import 'infra/database/db_configuration.dart';
 import 'infra/dependency_injector/injects.dart';
 import 'infra/middleware_interception.dart';
 import 'utils/custom_env.dart';
@@ -12,17 +11,11 @@ import 'utils/custom_env.dart';
 void main() async {
   CustomEnv.fromFile('.env-dev');
 
-  var conexao = MySqlConnection.connect(
-    ConnectionSettings(
-      host: await CustomEnv.get<String>(key: 'db_host'),
-      port: await CustomEnv.get<int>(key: 'db_port'),
-      user: await CustomEnv.get<String>(key: 'db_user'),
-      password: await CustomEnv.get<String>(key: 'db_pass'),
-      db: await CustomEnv.get<String>(key: 'db_schema'),
-    ),
-  );
-
   final _di = Injects.initialize();
+
+  var conexao = await _di.get<DBConfiguration>().connection;
+  var result = await conexao.query('SELECT * FROM usuarios;');
+  print(result);
 
   var cascateHandler = Cascade()
       .add(_di.get<LoginApi>().getHandler())
