a25fc6525bde17491582976a19078ff041d924d6
Emílio Nicoletti <eamnicoletti@gmail.com>
2023-02-01 21:49:21 -0300

    refactor: noticia model

--- bin/models/noticia_model.dart
+++ bin/models/noticia_model.dart
@@ -1,45 +1,16 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class NoticiaModel {
-  final int? id;
-  final String titulo;
-  final String descricao;
-  final String imagem;
-  final DateTime dtPublicacao;
-  final DateTime? dtAtulizacao;
+  int? id;
+  String? title;
+  String? description;
+  DateTime? dtCreated;
+  DateTime? dtUpdated;
+  int? userId;
 
-  NoticiaModel({
-    this.id,
-    required this.titulo,
-    required this.descricao,
-    required this.imagem,
-    required this.dtPublicacao,
-    this.dtAtulizacao,
-  });
+  NoticiaModel();
 
   @override
   String toString() {
-    return 'NoticiaModel(id: $id, titulo: $titulo, descricao: $descricao, imagem: $imagem, dtPublicacao: $dtPublicacao, dtAtulizacao: $dtAtulizacao)';
-  }
-
-  factory NoticiaModel.fromJson(Map map) {
-    return NoticiaModel(
-      id: map['id'] ?? '',
-      titulo: map['titulo'],
-      descricao: map['descricao'],
-      imagem: map['imagem'],
-      dtPublicacao: DateTime.fromMicrosecondsSinceEpoch(map['dtPublicacao']),
-      dtAtulizacao: map['dtAtulizacao'] != null
-          ? DateTime.fromMicrosecondsSinceEpoch(map['dtAtulizacao'])
-          : null,
-    );
-  }
-
-  Map toJson() {
-    return {
-      "id": id,
-      "titulo": titulo,
-      "descricao": descricao,
-      "imagem": imagem,
-    };
+    return 'NoticiaModel(id: $id, title: $title, description: $description, dtCreated: $dtCreated, dtUpdated: $dtUpdated, userId: $userId)';
   }
 }
